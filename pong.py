d9ff7042e0d7566801072d0e6e6d19333b6e988c
Marc Bernstein <marcbernstein98@gmail.com>
2024-03-16 18:15:21 -0400

    add basic screen make

--- pong.py
+++ pong.py
@@ -1,5 +1,25 @@
+from typing import Tuple
+import pygame
+
+
+def makeScreen(backgroundColor: Tuple[int, int, int], caption: str) -> pygame.Surface:
+    screen = pygame.display.set_mode((300, 300)) 
+    pygame.display.set_caption(caption) 
+    screen.fill(backgroundColor);
+    pygame.display.flip() 
+    return screen
+
 def main():
-    
+    screen = makeScreen((234, 212, 252), "PONG")
+    # Update the display using flip 
+  
+    # Variable to keep our game loop running 
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
     print("Hello world")
 
 
